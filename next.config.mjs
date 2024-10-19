fcfbe0aa8649d83705076e5227003da26c1b2ac4
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-18 20:40:08 -0400

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    localPatterns: [
+      {
+        pathname: "./src/images/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
