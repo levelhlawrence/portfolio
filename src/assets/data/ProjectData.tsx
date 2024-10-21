aa2a9673e3f8704c25c6af0f90e558c15070fb9e
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-21 01:52:54 -0400

    portfolio page in progress

---
+++ src/assets/data/ProjectData.tsx
@@ -0,0 +1,9 @@
+import pacesImage from "../images/pacesResortPhoto.png";
+
+export const ProjectData = [
+  {
+    title: "Paces Retreat",
+    tech_used: "SASS, ReactJS",
+    image: pacesImage,
+  },
+];
