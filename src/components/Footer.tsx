fecba7ed2f2ec9c8c635dc37bbb0bc11b703034b
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-23 22:45:23 -0400

    corrected deployment errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,9 @@
-import { FaLinkedin } from "react-icons/fa";
-import { IoNewspaperSharp } from "react-icons/io5";
-import { FaGithub } from "react-icons/fa";
 import { siteNavLinks, jobLinks } from "../components/Links";
 import { v4 as uuidv4 } from "uuid";
 import Link from "next/link";
 export default function Footer() {
   return (
-    <footer className="bg-black flex justify-center text-white mt-auto py-4  text-gray-400">
+    <footer className="bg-black flex justify-center mt-auto py-4  text-gray-400">
       <div className="flex w-full justify-center flex-col items-center align-middle max-w-3xl">
         <Link href="/" className="font-bold">
           levellawrence.net &copy; 2024
