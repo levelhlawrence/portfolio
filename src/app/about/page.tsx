d0ee7f645861c230cc03e2a938d23451cfa112e1
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-23 23:27:42 -0400

    updated page styling

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,26 @@
 export default function Page() {
   return (
-    <section className="flex justify-center mt-8 px-6">
+    <section className="flex justify-center mt-24 px-6 text-center md:text-left">
       <div className=" max-w-3xl">
-        <h1 className="text-3xl font-bold mb-4 text-center md:text-left">
-          About Me
+        <h1 className="text-5xl font-bold mb-4 ">
+          <span className="text-gray-500">About</span> Me
         </h1>
         <p>
-          Hello! I&apos;m a passionate full-stack developer with a knack for
-          turning ideas into functional and user-friendly web applications. With
-          a background in the military, I&apos;ve honed discipline, teamwork,
-          and problem-solving skills that I bring to every project. By
-          incorporating business contextual graphics into my work, I create
-          visually engaging interfaces that effectively communicate complex
-          information and enhance user experience. With a strong foundation in
-          both front-end and back-end technologies like JavaScript, React,
-          Node.js, and Python, I enjoy the challenge of crafting seamless
-          digital experiences.
+          <span className="font-bold">Hello!</span> I&apos;m a passionate
+          full-stack developer with a knack for turning ideas into functional
+          and user-friendly web applications. With a background in the military,
+          I&apos;ve honed discipline, teamwork, and problem-solving skills that
+          I bring to every project. By incorporating business contextual
+          graphics into my work, I create visually engaging interfaces that
+          effectively communicate complex information and enhance user
+          experience. With a strong foundation in both front-end and back-end
+          technologies like{" "}
+          <span className="text-yellow-600 font-bold">JavaScript</span>,{" "}
+          <span className="text-blue-600 font-bold">ReactJS</span>,{" "}
+          <span className="text-green-600 font-bold">NodeJS</span>, and{" "}
+          <span className="text-yellow-600 font-bold">Py</span>
+          <span className="text-gray-500 font-bold">thon</span>, I enjoy the
+          challenge of crafting seamless digital experiences.
         </p>
         <p className="my-4">
           Whether I&apos;m building interactive user interfaces or developing
