e17ff60cec0cd5f9975d10d2c9accff4950028c4
LawrenceLevel-FS <levelhlawrence@gmail.com>
2024-10-25 22:59:00 -0400

    nav padding corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <section className="flex justify-center items-center px-6 mt-24 md:mt-40 ">
+    <section className="flex justify-center items-center px-6 mt-24 md:mt-40 relative">
       <div className="max-w-3xl mb-16 text-center md:text-left">
         <p className="text-lg md:text-xl font-semibold text-gray-500 mb-4">
           Hi, I&apos;m Level Lawrence
